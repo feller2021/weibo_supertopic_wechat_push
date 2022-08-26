60457397a0e608736cd8be02e3fd244343675dbf
Feller <iamliurenjie@gmail.com>
2022-08-26 10:16:05 +0800

    Update htmljiexi.py

--- htmljiexi.py
+++ htmljiexi.py
@@ -262,7 +262,7 @@ def iphonepushdeer(fasname,idd):
     pushdeer = PushDeer(pushkey=pushkey)
     
     print(tpp4)
-        if tpp4=='':
+    if tpp4=='':
         print("无图片")
         return
     else:
